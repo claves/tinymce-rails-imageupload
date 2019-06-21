26db42f39cccfe92c3631b458f756a8f21bd5259
Masakazu Seto <masakazu.seto@claves.co.jp>
2019-06-21 15:15:06 +0900

    add ja.js.

---
+++ app/assets/javascripts/tinymce/plugins/uploadimage/langs/ja.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('ja', {
+  'Insert an image from your computer': 'ローカルの画像をアップロードする',
+  'Insert image': '画像を挿入',
+  'Choose an image': "画像を選択",
+  'You must choose a file': "ファイルを選択してください",
+  'Got a bad response from the server': "サーバからエラーを受け取りました",
+  "Didn't get a response from the server": "サーバが応答しません",
+  'Insert': "挿入",
+  'Cancel': "キャンセル",
+  'Image description': "画像の説明",
+});
